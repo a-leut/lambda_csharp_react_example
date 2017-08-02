a7c866790ff11c2d2aac9aa1e49ce095dfead5d0
aleut <turbomonkeyninja@gmail.com>
2017-08-01 22:53:33 -0600

    kind of working, had it for a sec

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {'response': 'none', 'value': ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  getUppercase(input) {
+    axios.post('https://txsca3462b.execute-api.us-west-2.amazonaws.com/deployed/mydemoresource', input)
+      .then(response => {
+        return response.data;
+      })
+      .catch(exception => {
+        return 'error';
+      });
+  }
+  handleChange(event) {
+    this.setState({'value': event.target.value});
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    var uc = this.getUppercase(this.state.value);
+    this.setState({'response': uc});
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Test Lambda App</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Input string
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          Response: {this.state.response}
       </div>
     );
   }
